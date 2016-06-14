de54beac2d273e9b0c3f48a1b1a3a50dd11e6788
Joshua Adeyemi <joshua.adeyemi@oneuni.co>
2016-06-14 13:14:17 -0700

    improve loading of role permissions

--- rolepermissions/loader.py
+++ rolepermissions/loader.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.conf import settings
+from django.apps import apps
 from importlib import import_module
 
 
@@ -8,7 +9,8 @@ def load_roles_and_permissions():
     if hasattr(settings, 'ROLEPERMISSIONS_MODULE'):
         import_module(settings.ROLEPERMISSIONS_MODULE)
 
-    for app_name in settings.INSTALLED_APPS:
+    for app in apps.get_app_configs():
+        app_name = app.name
         if app_name is not 'rolepermissions':
             try:
                 import_module('.permissions', app_name)
